7ef77a951b31edd495c2ace8835ec08a7fc3bf1c
Cloud User <centos@ip-172-31-20-251.eu-west-2.compute.internal>
2020-03-13 00:28:58 +0000

    AMi

---
+++ data_source/ec2.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = "us-west-2"
+}
+#Gets ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+
+output "UBUNTU_AMI_ID" {
+    value = "${data.aws_ami.ubuntu.id}"
+}
+
+#Gets Centos AMI
+data "aws_ami" "centos" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["679593333241"] # Canonical
+}
+
+
+
+output "CENTOS_AMI_ID" {
+    value = "${data.aws_ami.centos.id}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
